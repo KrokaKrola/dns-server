039c009347edd041f9e5fcf1f055a9989de39222
KrokaKrola <harlamsan@gmail.com>
2023-12-21 22:23:46 +0200

    stage 7

--- app/main.js
+++ app/main.js
@@ -12,28 +12,32 @@ class DnsMessage {
 
     /**
      *
-     * @type {DnsQuestion[]}
+     * @type {DnsQuestions}
      */
-    questions = [];
+    dnsQuestions = null;
 
     /**
      *
-     * @type {DnsAnswer}
+     * @type {DnsAnswer[]}
      */
-    dnsAnswer = null;
+    dnsAnswers = null;
 
-    constructor(dnsHeader, dnsQuestions, dnsAnswer) {
+    constructor(dnsHeader, dnsQuestions, dnsAnswers) {
         this.header = dnsHeader;
-        this.questions = dnsQuestions;
-        this.dnsAnswer = dnsAnswer;
+        this.dnsQuestions = dnsQuestions;
+        this.dnsAnswers = dnsAnswers;
     }
 
     packMessage() {
         const header = new Uint8Array(this.header.packHeaderForResponse());
-        const question = new Uint8Array(this.questions[0].packQuestionForResponse());
-        const answer = new Uint8Array(this.dnsAnswer.packAnswerForResponse());
+        const questions = this.dnsQuestions.questions.map(q => {
+            return new Uint8Array(this.dnsQuestions.packQuestionForResponse(q.qname, q.qtype, q.qclass));
+        })
+        const answers = this.dnsAnswers.map(a => {
+            return new Uint8Array(a.packAnswerForResponse());
+        });
 
-        return Buffer.concat([header, question, answer]);
+        return Buffer.concat([header, ...questions, ...answers]);
     }
 
 }
@@ -48,8 +52,7 @@ class DnsHeader {
     ra = 0;
     z = 0;
     rcode = 0;
-    // assuming that we always have 1 question
-    qdcount = 1;
+    qdcount = 0;
     ancount = 1;
     nscount = 0;
     arcount = 0;
@@ -60,32 +63,49 @@ class DnsHeader {
      */
     constructor(buf) {
         this.id = buf.readUInt16BE(0);
-        const flags = buf.readUInt16BE(2);
-        this.qr = flags >> 15;
-        this.opcode = (flags >> 11) & 0b1111;
-        this.aa = (flags >> 10) & 1;
-        this.tc = (flags >> 9) & 1;
-        this.rd = (flags >> 8) & 1;
-        this.ra = (flags >> 7) & 1;
-        this.z = (flags >> 4) & 0b111;
-        this.rcode = flags & 0b1111;
+        const flags = this.#parseFlagsByte(buf.readUInt16BE(2));
+        this.qr = flags.qr;
+        this.opcode = flags.opcode;
+        this.aa = flags.aa;
+        this.tc = flags.tc;
+        this.rd = flags.rd;
+        this.ra = flags.ra;
+        this.z = flags.z;
+        this.rcode = flags.rcode;
+        this.qdcount = buf.readUInt16BE(4);
+        this.ancount = this.qdcount;
     }
 
-    packHeaderForResponse() {
-        const buf = Buffer.alloc(12);
-        const rCode = 4;
-        buf.writeUInt16BE(this.id, 0);
-        buf.writeUInt16BE(
-            (1 << 15) |
+    #parseFlagsByte(byte) {
+        return {
+            qr: byte >> 15,
+            opcode: (byte >> 11) & 0b1111,
+            aa: (byte >> 10) & 1,
+            tc: (byte >> 9) & 1,
+            rd: (byte >> 8) & 1,
+            ra: (byte >> 7) & 1,
+            z: (byte >> 4) & 0b111,
+            rcode: byte & 0b1111,
+        };
+    }
+
+    #packFlagsByte(qr, rcode) {
+        return (qr << 15) |
             (this.opcode << 11) |
             (this.aa << 10) |
             (this.tc << 9) |
             (this.rd << 8) |
             (this.ra << 7) |
             (this.z << 4) |
-            rCode,
-            2
-        );
+            rcode;
+    }
+
+    packHeaderForResponse() {
+        const buf = Buffer.alloc(12);
+        const rCode = 4;
+        const qr = 1;
+        buf.writeUInt16BE(this.id, 0);
+        buf.writeUInt16BE(this.#packFlagsByte(qr, rCode), 2);
         buf.writeUInt16BE(this.qdcount, 4);
         buf.writeUInt16BE(this.ancount, 6);
         buf.writeUInt16BE(this.nscount, 8);
@@ -94,38 +114,36 @@ class DnsHeader {
     }
 }
 
-class DnsQuestion {
-    qname = "";
-    qtype = 0;
-    qclass = 0;
+class DnsQuestions {
+    #questions = [];
+    questionCount = 0;
 
-    /**
-     *
-     * @param buf {Buffer}
-     */
-    constructor(buf) {
+    constructor(packetBuffer, questionCount) {
+        this.questionCount = questionCount;
+        this.parse(packetBuffer);
+    }
 
-        let offset = 0;
-        let length = buf.readUInt8(offset);
-        const labels = [];
+    #parseDomainName(packet, offset) {
+        let domain = '';
+        let length = packet.readUInt8(offset);
 
         while (length !== 0) {
-            // increment offset because length of label is stored in first byte
-            offset++;
-            labels.push(buf.toString('utf-8', offset, offset + length));
-            offset += length;
-            length = buf.readUInt8(offset);
+            domain += packet.toString('utf-8', offset + 1, offset + 1 + length) + '.';
+            offset += length + 1;
+            length = packet.readUInt8(offset);
         }
 
-        this.qname = labels.join('.');
-        // offset to move over empty byte \x00
-        offset++;
+        domain = domain.slice(0, -1); // Remove the last dot
 
-        this.qclass = buf.readUInt16BE(offset);
+        return {domain, offset: offset + 1};
+    }
 
-        // qclass = 2 byte, move offset
-        offset += 2;
-        this.qtype = buf.readUInt16BE(offset);
+    #parseQuestion(packet, offset) {
+        const {domain, offset: newOffset} = this.#parseDomainName(packet, offset);
+        const qtype = packet.readUInt16BE(newOffset);
+        const qclass = packet.readUInt16BE(newOffset + 2);
+
+        return {qname: domain, qtype, qclass, offset: newOffset + 4};
     }
 
     #encodeDomainName(domain) {
@@ -143,16 +161,49 @@ class DnsQuestion {
         return buffer;
     }
 
-    packQuestionForResponse() {
-        const domain = this.#encodeDomainName(this.qname);
+    packQuestionForResponse(qname, qtype, qclass) {
+        const domain = this.#encodeDomainName(qname);
         const buffer = Buffer.alloc(domain.length + 4); // +4 for the 2 bytes of qtype and 2 bytes of qclass
         domain.copy(buffer); // copy the encoded domain name to the buffer
         let offset = domain.length;
-        buffer.writeUInt16BE(this.qtype, offset);
+        buffer.writeUInt16BE(qtype, offset);
         offset += 2;
-        buffer.writeUInt16BE(this.qclass, offset);
-
+        buffer.writeUInt16BE(qclass, offset);
+        // console.log(qname, buffer);
         return buffer;
+    }
+
+    parse(packetBuffer) {
+        let offset = 0; // Skip DNS header
+
+        const that = this;
+
+        if (this.questionCount === 1) {
+            const question = this.#parseQuestion(packetBuffer, offset);
+            this.#questions.push(question);
+        } else {
+            const question = this.#parseQuestion(packetBuffer, offset);
+            this.#questions.push(question);
+            offset = question.offset;
+
+            while (that.#questions.length < that.questionCount) {
+                if (offset >= packetBuffer.length) {
+                    break;
+                }
+
+                const byte = packetBuffer.readUInt8(offset);
+                if ((byte & 0xC0) === 0xC0) {
+                    const pointerOffset = (byte << 8) | packetBuffer.readUInt8(offset + 1);
+                    this.#questions.push(this.#parseQuestion(packetBuffer, (pointerOffset & 0x3FFF) - 16));
+                } else {
+                    offset++;
+                }
+            }
+        }
+    }
+
+    get questions() {
+        return this.#questions;
     }
 }
 
@@ -208,12 +259,13 @@ class DnsAnswer {
 
 udpSocket.on("message", (buf, rinfo) => {
     try {
-        console.log('done previously 🙂')
         const dnsHeader = new DnsHeader(buf.subarray(0, 12));
-        const dnsQuestion = new DnsQuestion(buf.subarray(12));
-        const dnsAnswer = new DnsAnswer(dnsQuestion.qname);
+        const dnsQuestion = new DnsQuestions(buf.subarray(12), dnsHeader.qdcount);
+        const dnsAnswers = dnsQuestion.questions.map(q => {
+            return new DnsAnswer(q.qname);
+        });
 
-        const message = new DnsMessage(dnsHeader, [dnsQuestion], dnsAnswer);
+        const message = new DnsMessage(dnsHeader, dnsQuestion, dnsAnswers);
         udpSocket.send(message.packMessage(), rinfo.port, rinfo.address);
     } catch (e) {
         console.log(`Error receiving data: ${e}`);
